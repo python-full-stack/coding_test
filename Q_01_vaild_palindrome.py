3b2454b5e2aa22038d1c1aa67e0ad60592d36b1e
jyc <genii0627@gmail.com>
2020-09-13 21:37:38 +0900

    added

---
+++ Q_01_vaild_palindrome.py
@@ -0,0 +1,12 @@
+import re
+def isPalindrome(s):
+    s = s.lower()
+
+    s = re.sub('[^a-z0-9]', '', s)
+
+    return s == s[::-1]
+
+input_data = " A man, a plan, a canal : Panama"
+
+result = isPalindrome(input_data)
+print(result)
