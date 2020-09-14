83a558d7cc9cac7159eaa76bbcf64e20295aa7dd
yang-yeongbo <ybyang2@lotte.net>
2020-09-15 00:44:18 +0900

    diddudqh : 1번 문제 풀이

--- Q_01_vaild_palindrome.py
+++ Q_01_vaild_palindrome.py
@@ -13,6 +13,20 @@ result = isPalindrome(input_data)
 print(result)
 # 영보 - start!
 
+#팰린드롬 : 앞뒤가 똑같은 단어나 문장
+import re
+
+class Solution :
+    def isPalindrom(self,s:str) -> bool:
+        s = s.lower()
+        #정규식으로 불필요한 문자 필터링
+        s = re.sub('[^a-z0-9]','',s)
+        return s == s[::-1]
+
+sample = 'A man, a plan, a canal: Panama'
+s = Solution()
+print(s.isPalindrom(sample))
+
 # 승범
 
 print("git push test")
